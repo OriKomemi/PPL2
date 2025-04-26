8ea3fa49fad5bb334cf6b88f49d3dafbe331b981
Ori Komemi <orikom123@gmail.com>
2025-04-26 16:22:33 +0300

    skeleton

---
+++ src/q3.ts
@@ -0,0 +1,16 @@
+import { Exp, Program } from './L3/L3-ast';
+import { Result, makeFailure} from './shared/result';
+
+/*
+Purpose: Transform L2 AST to JavaScript program string
+Signature: l2ToJS(l2AST)
+Type: [EXP | Program] => Result<string>
+*/
+
+export const l2ToJS = (exp: Exp | Program): Result<string>  =>
+    makeFailure("Not implemented yet");
+
+
+// implement pactorial in scheme
+//(define (pactorial n)
+// lamda(if (= n 0) 1 (* n (pactorial (- n 1)))))
